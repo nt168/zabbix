8019d9c18eb7a916e7ae1f106befdae7143f3b5e
nt168 <nt168@aliyun.com>
2026-01-09 16:33:03 +0800

    独立zbxipcservice子项

---
+++ tests/libs/zbxipcservice/nlib/include/config.h
@@ -0,0 +1,37 @@
+#ifndef ZBXIPCSERVICE_NLIB_CONFIG_H
+#define ZBXIPCSERVICE_NLIB_CONFIG_H
+
+#define HAVE_ASSERT_H 1
+#define HAVE_ARPA_INET_H 1
+#define HAVE_CTYPE_H 1
+#define HAVE_ERRNO_H 1
+#define HAVE_FCNTL_H 1
+#define HAVE_FLOAT_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_LIMITS_H 1
+#define HAVE_MATH_H 1
+#define HAVE_NETDB_H 1
+#define HAVE_NETINET_IN_H 1
+#define HAVE_POLL_H 1
+#define HAVE_SIGNAL_H 1
+#define HAVE_STDARG_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDIO_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_SYS_PARAM_H 1
+#define HAVE_SYS_RESOURCE_H 1
+#define HAVE_SYS_SOCKET_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TIME_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_UN_H 1
+#define HAVE_SYS_WAIT_H 1
+#define HAVE_TIME_H 1
+#define HAVE_UNISTD_H 1
+
+#define HAVE_IPCSERVICE 1
+#define HAVE_LIBEVENT 1
+#define HAVE_TIME_CLOCK_GETTIME 1
+#endif
